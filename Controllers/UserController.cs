a6ab627a47797b67a2c3c3eba6113fe0aadea026
Matheus Juski <juskimatheus@gmail.com>
2025-09-17 21:47:42 -0300

    Subindo projeto completo

---
+++ Controllers/UserController.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Mvc;
+using UserManagementAPI.Models;
+using UserManagementAPI.Services;
+
+namespace UserManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _service;
+
+        public UserController(IUserService service)
+        {
+            _service = service;
+        }
+
+        // GET: api/user
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _service.GetAllUsersAsync();
+            return Ok(users);
+        }
+
+        // GET: api/user/{id}
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var user = await _service.GetUserByIdAsync(id);
+            if (user == null) return NotFound();
+            return Ok(user);
+        }
+
+        // POST: api/user
+        [HttpPost]
+        public async Task<IActionResult> Create(User user)
+        {
+
+            var createdUser = await _service.CreateUserAsync(user);
+            return CreatedAtAction(nameof(GetById), new { id = createdUser.Id }, createdUser);
+        }
+
+        // PUT: api/user/{id}
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, User user)
+        {
+            var updated = await _service.UpdateUserAsync(id, user);
+            if (Update == null) return NotFound();
+            return NoContent();
+        }
+
+        // DELETE: api/user/{id}
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var deleted = await _service.DeleteUserAsync(id);
+            if (!deleted) return NotFound();
+            return NoContent();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -19,11 +19,15 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<IUserService, UserService>();
 
+builder.Services.AddControllers();
+
+
 
 var app = builder.Build();
 
 app.UseSwagger();
 app.UseSwaggerUI();
+app.MapControllers();
 
 app.MapGet("/", () => "UsermanagementAPI Funcionando!");
 
